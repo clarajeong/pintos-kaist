22549191004dcadc5ed01775d10a69a2615d7bb0
Euijin Shin <ejshin0310@kaist.ac.kr>
2022-09-18 18:41:35 +0900

    priority_preemption

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -162,6 +162,6 @@ int64_t get_next_tick_to_awake(void);
 void test_max_priority (void);
 bool cmp_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 
-
+void priority_preemption(void);
 
 #endif /* threads/thread.h */
